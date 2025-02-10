e4c8094c17858a97cf77b30171d35146fca2b92d
9999-balasu <138554962+9999-balasu@users.noreply.github.com>
2025-02-10 17:10:27 +0530

    change

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
+/*import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+})*/
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase the chunk size warning limit (default is 500 KB)
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Move node_modules into a separate chunk
+          }
+        },
+      },
+    },
+  },
+});
+
